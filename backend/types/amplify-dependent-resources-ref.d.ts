fba91eca56b3df95c7ebe8d222f1eb50b12c41b5
shaikust <106724610+shaikust@users.noreply.github.com>
2024-01-02 11:08:36 +0530

    Add files via upload

---
+++ backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "demolccapp1": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "demolccapp1749bd321": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "userstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
